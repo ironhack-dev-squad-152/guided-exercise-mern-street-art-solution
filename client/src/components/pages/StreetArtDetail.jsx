89e85d646bb95162b0ca0f457e07bfb2c2829e31
Maxence Bouret <maxence.bouret@gmail.com>
2019-09-24 16:47:02 +0200

    Iteration 13

--- client/src/components/pages/StreetArtDetail.jsx
+++ client/src/components/pages/StreetArtDetail.jsx
@@ -1,13 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import api from '../../api'
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl'
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
 
 export default function StreetArtDetail(props) {
   let streetArtId = props.match.params.streetArtId
   const [streetArt, setStreetArt] = useState(null)
+  const mapDomRef = useRef(null)
+  let map = useRef(null).current // A variable that is always the same, even after a new render
+  let marker = useRef(null).current // A variable that is always the same, even after a new render
 
-  api.getStreetArt(streetArtId).then(streetArtFromApi => {
-    setStreetArt(streetArtFromApi)
-  })
+  useEffect(() => {
+    api.getStreetArt(streetArtId).then(streetArtFromApi => {
+      setStreetArt(streetArtFromApi)
+      let [lng, lat] = streetArtFromApi.location.coordinates
+      initMap(lng, lat)
+    })
+  }, [])
+
+  function initMap(lng, lat) {
+    // Embed the map where "mapDomRef" is defined in the render
+    map = new mapboxgl.Map({
+      container: mapDomRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: 10,
+    })
+
+    // Add zoom control on the top right corner
+    map.addControl(new mapboxgl.NavigationControl())
+
+    // Create a marker on the map with the coordinates ([lng, lat])
+    marker = new mapboxgl.Marker({ color: 'red' })
+      .setLngLat([lng, lat])
+      .addTo(map)
+  }
 
   function handleImgClick(e) {
     e.target.requestFullscreen() // e.target => DOM element
@@ -21,6 +48,7 @@ export default function StreetArtDetail(props) {
       <hr />
       Longitude: {streetArt.location.coordinates[0]} <br />
       Latitude: {streetArt.location.coordinates[1]} <br />
+      <div ref={mapDomRef} style={{ height: 400 }}></div>
     </div>
   )
 }
